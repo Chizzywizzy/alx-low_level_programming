66bc1b14d86c509215639396c1b900ea23888a25
Chizzywizzy <ned.uz07@gmail.com>
2022-09-15 14:50:11 -0700

    Hello,please

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,20 @@
-#include <studio.h>
+#include <stdio.h>
 
 /**
- * main - Entry point
  *
- * Return: Always 0
- */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 
+ *
+ *     */
 
-	return (0);
+int main(void)
+
+{
+puts("\"Programming is like building a multilingual puzzle");
+	    return (0);
+
 }
 
